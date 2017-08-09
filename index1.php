ce55e5bab61cac9b3eb6872c32844c8a7341c5dc
Sven Luetje <sven.luetje@googlemail.com>
2017-08-09 21:27:10 +0200

    Initial commit of website

---
+++ index1.php
@@ -0,0 +1,29 @@
+<html>
+<meta name="author" content="St. Stephan Brass Band in Hamburg">
+<meta name="publisher" content="St. Stephan Brass Band in Hamburg">
+<meta name="copyright" content="St. Stephan Brass Band in Hamburg">
+<meta name="keywords" content="Brassband, Hamburg, Musik, Filmmusik, Amateurorchester, Konzert, Konzerte, Veranstalter, Konzertveranstalter, Agenturen, Konzertagentur, Veranstaltungen">
+<meta name="description" content="Homepage der St. Stephan Brass Band aus Hamburg">
+<meta name="page-topic" content="Musik, Kultur">
+<title>St. Stephan Brass Band Hamburg</title>
+<meta http-equiv="CACHE-CONTROL" content="NO-CACHE">
+<meta http-equiv="PRAGMA" content="NO-CACHE">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="robots" content="noindex">
+<frameset frameborder="0" border="0" framespacing="0"> 
+
+<frameset frameborder="0" border="0" framespacing="0"> 
+
+	<frame name="home" noresize src="fwd.html">
+   <?php
+   $arg = $_SERVER['QUERY_STRING'];
+   if($arg == "") $arg = "fwd.html";
+   print "<frame id=main name=main LeftMargin=0  marginWidth=0 marginHeight=0 frameBorder=0 src=\"$arg\" name=\"main\">";
+   ?>
+	
+    <noframes>
+		<body></body>
+    </noframes>
+</frameset>
+</frameset>
+</html>
